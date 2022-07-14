e8fdf3b986d80a9c1ed982933928104d06205c31
William Theesfeld <william@theesfeld.net>
2022-07-14 12:47:41 -0400

    working on RTT/UPPRT

--- elcal.py
+++ elcal.py
@@ -11,22 +11,23 @@ class Form(QDialog):
         self.capacityFactorLabel = QLabel('Capacity Factor (%)')
         self.capacityFactor = QSpinBox()
         self.capacityFactor.setMaximum(100)
-        self.elevatorCapacityLabel = QLabel('Elevator Capacity (lb)')
-        self.elevatorCapacity = QSpinBox()
-        self.elevatorCapacity.setMaximum(20000)
+        self.elevatorTableLabel = QLabel('Elevator Capacity (lb)')
         self.estimateButton = QPushButton('Estimate')
         self.outputLabel = QLabel()
         self.outputLabel.clear()
         self.estimateButton.clicked.connect(self.estimate)
+        self.elevatorTable = QTableWidget(30,6)
+        self.elevatorTable.setHorizontalHeaderLabels(['Capacity', 'Speed', 'Acceleration', 'Jerk', 'Door Open', 'Door Close'])
         self.riserTable = QTableWidget(80,3)
         self.riserTable.setHorizontalHeaderLabels(['Floor', 'Height', 'Population'])
 
         grid = QGridLayout()
+        grid.addWidget(self.elevatorTableLabel,3,1)
+        grid.addWidget(self.elevatorTable,0,0,5,1)
+        grid.addWidget(self.riserTable,0,1,5,1)
+        
         grid.addWidget(self.capacityFactorLabel,2,1)
         grid.addWidget(self.capacityFactor,2,2)
-        grid.addWidget(self.elevatorCapacityLabel,3,1)
-        grid.addWidget(self.elevatorCapacity,3,2)
-        grid.addWidget(self.riserTable,0,0,5,1)
         grid.addWidget(self.estimateButton,5,1)
         grid.addWidget(self.outputLabel,5,2)
         grid.rowStretch(1)
@@ -38,56 +39,64 @@ class Form(QDialog):
         self.show()
 
     def estimate(self):
-
+        cf = self.capacityFactor.value()
+        cc = self.elevatorCapacity.value()
+        ui = []
+        j = 
+        n = []
         # ElevatorVariables.md has variable list
         
         # persons per elevator
-        # P = (CF/100) * CC
+        # p=\left(\frac{cf}{100}\right)\cdot{cc}
+        p = (cf/100) * cc
         
         # Effective Building population
         # Ueff = 0
         # for floorpopulation in list of floors/populations
         #   ueff = ueff + i
 
-        # Average Highest Reversal Floor
-        # H = 
+        # Average Highest Reversal Floor 
         # H=\sum_{j=1}^{N-1}\left(\sum_{i=1}^{j}\frac{U_i}{U_eff}\right)^P
+        h = sum(sum(ui/((ueff)), (i, 1, j))**p, (j, 1, n - 1))
 
         # Average Number Of Stops Made During Round Trip
         # S=N-\sum_{i=1}^{N}\left(1-\frac{U_i}{U_eff}\right)^P
+        s = n - sum((-ui/(ueff) + 1)**p, (i, 1, n))
 
         # Average Time For Passenger To Load And Unload The Elevator
-        # tp = (tl+tu)/2
+        # tp=\frac{({tl}+{tu})}{2}
+        tp = (tl+tu)/2
 
         # Time taken for the lift to travel between two adjacent floors at rated speed
-        # tv = df / v
+        # tv=\frac{df}{v}
+        tv = df / v
 
         # cycle time (travel to floor, open / close doors)
-        # T = tfl + tc + to
+        # t={tfl}+{tc}+{to}
+        t = tfl + tc + to
 
         # time consumed making a single stop
-        # ts = T - tv
+        # ts={t}-{tv}
+        ts = t - tv
 
         # Round Trip Time (RTT)
-        # RTT = (2*H*tv) + ((S + 1)*ts) + (2*P*tp)
+        # rtt=\left[\right]
+        rtt = (2*h*tv) + ((s + 1)*ts) + (2*p*tp)
 
         # Up Peak Round Trip Time (UPPRTT) [RTT with 'losses']
-        # UPPINT = RTT/L
+        # uppint=\frac{rtt}{l}
+        uppint = rtt/l
 
         # Up Peak Handling Capacity (UPPHC)
         # NUMBER of people moved in 5 minutes
-        # UPPHC = (300*(P*L))/RTT
+        # upphc=\frac{300\cdot{p}\cdot{*}l}{rtt}   
+        upphc = 300*p*l/rtt
 
         # Handling Capacity
         # PERCENT of people moved in 5 minutes
-        # HC = (UPPHC*100)/ueff
+        # hc=\frac{{upphc}\cdot{100}}{ueff}
+        hc = (upphc*100)/ueff
 
-        #A = self.areaPerFloor.value()*10.764
-        #F = self.buildingFloors.value()
-        #C = float((self.capacityFactor.value()/100))
-        #E = (self.elevatorCapacity.value()*C) / 165
-        #N = (A*F*C)/(226*E)
-        #self.outputLabel.setText(f'{round(N)}')
         print(f'Calculating')
 
 if __name__ == "__main__":
